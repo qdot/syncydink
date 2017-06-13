9e1f5b16a95b2a2a3defb0591145e46c3f299999
Kyle Machulis <kyle@nonpolynomial.com>
2017-06-12 21:31:31 -0700

    feat: Added new component skeletons
    I honestly have no idea what I am doing at this point but hopefully
    this is the right way to set up components.

---
+++ src/components/ButtplugDeviceManager/ButtplugDeviceManager.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="buttplug-device-manager">
+    <ul id="buttplug-device-list">
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" src="./ButtplugDeviceManager.ts">
+</script>
+
+<style lang="less" src="./ButtplugDeviceManager.less">
+</style>
---
+++ src/components/ButtplugLogManager/ButtplugLogManager.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="buttplug-log-manager">
+    <textarea name="buttplug-log-area"></textarea>
+    <select name="buttplug-log-level">
+      <option value="Off">Off</option>
+      <option value="Fatal">Fatal</option>
+      <option value="Error">Error</option>
+      <option value="Warn">Warn</option>
+      <option value="Info">Info</option>
+      <option value="Debug">Debug</option>
+      <option value="Trace">Trace</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts" src="./ButtplugLogManager.ts">
+</script>
+
+<style lang="less" src="./ButtplugLogManager.less">
+</style>
