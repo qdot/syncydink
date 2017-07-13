19dac7114e948a5c49d3c32ef349ee8b9ab992df
Kyle Machulis <kyle@nonpolynomial.com>
2017-07-13 01:04:44 -0700

    feat: Add funscript to FleshlightLaunch conversation
    Converts funscript commands into buttplug fleshlight launch commands.

---
+++ src/components/SyncyDinkVideo/HapticsToButtplug.ts
@@ -0,0 +1,41 @@
+import { FleshlightLaunchFW12Cmd } from "buttplug";
+import { FunscriptCommand, HapticFileHandler } from "haptic-movie-file-reader";
+
+export default class HapticCommandToButtplugMessage {
+  public static FunScriptToFleshlightLaunchCommands(aCommands: FunscriptCommand[]):
+  Map<number, FleshlightLaunchFW12Cmd> {
+    let lastTime: number = 0;
+    let lastPosition: number = 0;
+
+    // TODO: We should make a way to change this somehow
+    const range: number = 90;
+    const commands: Map<number, FleshlightLaunchFW12Cmd> = new Map();
+    console.log("Got " + aCommands.length + " commands");
+    for (const aCommand of aCommands) {
+      // if this is our first element, save off and continue.
+      if (lastTime < 0) {
+        lastTime = aCommand.Time;
+        lastPosition = aCommand.Position;
+        continue;
+      }
+
+      const currentTime = aCommand.Time;
+      const currentPosition = aCommand.Position;
+
+      const duration = currentTime - lastTime;
+
+      const positionDelta = Math.abs(currentPosition - lastPosition);
+      let speed = Math.floor(25000 * Math.pow(((duration * 90) / positionDelta), -1.05));
+
+      // Clamp speed on 20 <= x <= 80 so we don't crash or break the launch.
+      speed = Math.min(Math.max(speed, 20), 80);
+
+      const positionGoal = Math.floor(((currentPosition / 99) * range) + ((99 - range) / 2));
+      commands.set(aCommand.Time, new FleshlightLaunchFW12Cmd(speed, positionGoal));
+      lastTime = aCommand.Time;
+      lastPosition = aCommand.Position;
+    }
+    console.log("Created " + commands.size + " commands");
+    return commands;
+  }
+}
