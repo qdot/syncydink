bea985b6174c659c8f174622f2ff0044ef6445fb
Kyle Machulis <kyle@nonpolynomial.com>
2017-07-16 19:23:49 -0700

    fix: Fix model for client name
    clientName not clientname

--- src/components/ButtplugConnectionManager/ButtplugConnectionManager.vue
+++ src/components/ButtplugConnectionManager/ButtplugConnectionManager.vue
@@ -2,7 +2,7 @@
   <div id="buttplug-connection-manager" class="buttplug-sidebar">
     <md-input-container v-if="!isConnected">
       <label id="form-label">Client Name</label>
-      <md-input class="form-text md-block" v-model="clientname" placeholder="Syncydink Video Client"/>
+      <md-input class="form-text md-block" v-model="clientName" placeholder="Syncydink Video Client"/>
     </md-input-container>
     <md-input-container v-if="!isConnected">
       <label id="form-label">Server Address</label>
