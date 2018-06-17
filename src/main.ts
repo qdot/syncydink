efe0c95b74d4f492e0b1c9fdb356c6c148a31515
Kyle Machulis <kyle@nonpolynomial.com>
2018-06-16 17:37:50 -0700

    chore: Update matomo server location

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const MatomoTracker = require("matomo-tracker");
 require("viewport-units-buggyfill").init();
 
 // Initialize with your site ID and Matomo URL
-const matomo = new MatomoTracker(11, "https://apps.nonpolynomial.com/p/js/", true);
+const matomo = new MatomoTracker(11, "https://matomo.nonpolynomial.com/", true);
 
 // Optional: Respond to tracking errors
 matomo.on("error", function(err: string) {
