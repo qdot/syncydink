19818dc09c29299eb6dcaac545da6d1c4408161d
Kyle Machulis <kyle@nonpolynomial.com>
2018-06-16 18:51:01 -0700

    chore: Try embedding matomo until we can get package to work

--- src/App.vue
+++ src/App.vue
@@ -127,6 +127,23 @@
         </md-tabs>
       </md-sidenav>
     </v-touch>
+    <!-- Matomo -->
+    <script type="text/javascript">
+     var _paq = _paq || [];
+     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+     _paq.push(["setCookieDomain", "*.buttplug.world"]);
+     _paq.push(['trackPageView']);
+     _paq.push(['enableLinkTracking']);
+     (function() {
+       var u="//matomo.nonpolynomial.com/";
+       _paq.push(['setTrackerUrl', u+'piwik.php']);
+       _paq.push(['setSiteId', '11']);
+       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+       g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+     })();
+    </script>
+    <noscript><p><img src="//matomo.nonpolynomial.com/piwik.php?idsite=11&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
   </div>
 </template>
 
