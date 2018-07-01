91023b5b8659ced142c41df88f34379bdf9bfc9f
Kyle Machulis <kyle@nonpolynomial.com>
2018-06-30 21:27:40 -0700

    feat: Codesplit VR player
    Only load VR player when needed.
    
    Fixes #54

--- src/main.ts
+++ src/main.ts
@@ -45,3 +45,11 @@ new Vue({
   el: "#app",
   render: (h) => h(App),
 });
+
+// Since aframe-video-player is a subcomponent, we can register it as part of
+// our component here, then delay load it as needed.
+// tslint:disable max-line-length
+Vue.component("aframe-video-player",
+              () => import (/* webpackChunkName: "syncydinkvrvideoplayer" */ "./components/AframeVideoPlayer/AframeVideoPlayer.vue" ));
+// Video Player and Encoder only take up about 250k before gzipping, not worth
+// codesplitting
