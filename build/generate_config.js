f1b3ce197bc46b3ba1b9b910e2a0ac471ce0ce4e
Kyle Machulis <kyle@nonpolynomial.com>
2018-07-04 15:49:43 -0700

    chore: Add short date string generator

--- build/generate_config.js
+++ build/generate_config.js
@@ -6,9 +6,17 @@ const revision = require('child_process')
       .execSync('git rev-parse --short HEAD')
       .toString().trim();
 
+function pad(str, amount) {
+  return ("0000" + str).slice(-amount);
+}
+
+const currentDate = new Date();
+const currentDateString = `${currentDate.getUTCFullYear()}/${pad(currentDate.getMonth(), 2)}/${pad(currentDate.getDay(), 2)} ${pad(currentDate.getHours(), 2)}:${pad(currentDate.getMinutes(), 2)} PT`;
+
 const config = {
   build_commit: revision,
   build_date: Date(),
+  short_build_date: currentDateString,
   buttplug_version: buttplug.version,
   component_version: vuecomponent.version
 };
